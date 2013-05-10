8d41a696821e24b25d0efe6930b0f757b062646c
norcasitas <nico.orcasitas@gmail.com>
2013-05-10 10:26:42 -0300

    Alta y baja Owner

--- src/main/java/com/unrc/app/Inmo.java
+++ src/main/java/com/unrc/app/Inmo.java
@@ -4,39 +4,40 @@ package com.unrc.app;
 import org.javalite.activejdbc.Base;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.unrc.app.models.Address;
-import com.unrc.app.models.City;
-import com.unrc.app.models.Owner;
-
+import com.unrc.app.object.*;
 
 public class Inmo {
 	 final static Logger logger = LoggerFactory.getLogger(Inmo.class);
 	 
 
-		
-	
     public static void main( String[] args )
     {
         Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/inmoapp_development", "root", "root");
-        City cit= new City();
-        cit.set("name","Cordoba","code",5800);
-        if(!cit.existCity()){
-        	cit.saveIt();
-        }
-        Address ad= new Address();
-        ad.set("city_id",City.findFirst("code = ?", 5800).getId(),"street","Arrayanes","num",2459,"neighborhood","Bimaco"); 
-        if(!ad.existAddress()){
-        	ad.saveIt();
-        	
-        }
-        Owner owner= new Owner();
-        System.out.println(ad.getInteger("id"));
-        owner.set("first_name","Nicolás","last_name","Orcasitas","dni","37.134.136","address_id",6);
-        System.out.println("Existe owner? : "+owner.existOwner());
-        if(!owner.existOwner()){
-        	owner.saveIt();
-        }
-        System.out.println( "Hello World!" );
+        ABMOwner create = new ABMOwner();
+        
+        ObjectOwner nico = new ObjectOwner("Nicolas","Orcasitas","37.134.136","nico.orcasitas@gmail.com",
+        						"Cordoba",5000,"Emilio Lamarca",3816,"Urca");
+        create.createOwner(nico);
+        
+        ObjectOwner jazu = new ObjectOwner("Jacinto","Jaimez","38.312.543","example@gmail.com",
+				"Rio Cuarto",5800,"Sobremonte",2912,"Aceitera");
+        create.createOwner(jazu);
+        
+        ObjectOwner eze = new ObjectOwner("ezequiel","Zensich","36.534.098","kkjkjdas@homail.com",
+				"Rio Cuarto",5800,"Juan josé Paso",312,"Ferroviario");
+        create.createOwner(eze);
+        
+        ObjectOwner joako = new ObjectOwner("Joako","Heredia","28.534.098","example@example.com",
+        		"Rio Cuarto",5800,"Sobremonte",2912,"Aceitera");
+        create.createOwner(joako);
+        
+        ObjectOwner example = new ObjectOwner("dasdas","eqweq","28.534.098","example@gmail.com",
+				"Rio Cuarto",5800,"Sobremonte",2912,"Ferroviario");
+        create.createOwner(example);//No se debe cargar porque tiene el mismo numero de documento que tuViejaAlta
+       
+       create.deleteOwner(joako);
+       
+
+        System.out.println( "¡SE EJECUTÓ TODO!" );
     }
 }
---
+++ src/main/java/com/unrc/app/object/ObjectOwner.java
@@ -0,0 +1,179 @@
+package com.unrc.app.object;
+
+public class ObjectOwner {
+	private String firstName;//Nombre
+	private String lastName;//Apellido
+	private String dni;//dni
+	private String email;//email
+	private String city;//ciudad
+	private int code;//codigo postal
+	private String street;//calle
+	private int num;//numero de domicilio
+	private String neighborhood;//barrio
+	
+
+	//Constructor vacio
+	public ObjectOwner() {
+		firstName=null;
+		lastName=null;
+		dni=null;
+		email=null;
+		street=null;
+		num=-1;
+		neighborhood=null;
+		city=null;
+		code=-1;
+	}
+
+	/**
+	 * @param firstName
+	 * @param lastName
+	 * @param dni
+	 * @param email
+	 * @param city
+	 * @param code
+	 * @param street
+	 * @param num
+	 * @param neighborhood
+	 */
+	public ObjectOwner(String firstName, String lastName, String dni,
+			String email, String city, int code, String street, int num,
+			String neighborhood) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.dni = dni;
+		this.email = email;
+		this.city = city;
+		this.code = code;
+		this.street = street;
+		this.num = num;
+		this.neighborhood = neighborhood;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the dni
+	 */
+	public String getDni() {
+		return dni;
+	}
+
+	/**
+	 * @param dni the dni to set
+	 */
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the street
+	 */
+	public String getStreet() {
+		return street;
+	}
+
+	/**
+	 * @param street the street to set
+	 */
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	/**
+	 * @return the num
+	 */
+	public int getNum() {
+		return num;
+	}
+
+	/**
+	 * @param num the num to set
+	 */
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	/**
+	 * @return the neighborhood
+	 */
+	public String getNeighborhood() {
+		return neighborhood;
+	}
+
+	/**
+	 * @param neighborhood the neighborhood to set
+	 */
+	public void setNeighborhood(String neighborhood) {
+		this.neighborhood = neighborhood;
+	}
+
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+
+	/**
+	 * @param city the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	/**
+	 * @return the code
+	 */
+	public int getCode() {
+		return code;
+	}
+
+	/**
+	 * @param code the code to set
+	 */
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+}
