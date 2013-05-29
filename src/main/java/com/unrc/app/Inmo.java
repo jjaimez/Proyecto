1973858d167bcf77bbda309e0a42261a8916ccda
norcasitas <nico.orcasitas@gmail.com>
2013-05-29 17:55:27 -0300

    Version acomodada en git

--- src/main/java/com/unrc/app/Inmo.java
+++ src/main/java/com/unrc/app/Inmo.java
@@ -10,39 +10,82 @@
 
 package com.unrc.app;
 
-import org.javalite.activejdbc.Base;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import com.unrc.app.object.*;
+
+import static spark.Spark.*;
+
+import java.util.Iterator;
+
+
+import org.javalite.activejdbc.*;
+
+import com.unrc.app.models.Owner;
+import com.unrc.app.object.ObjectOwner;
+
+import spark.*;
+
 
 public class Inmo {
-	 final static Logger logger = LoggerFactory.getLogger(Inmo.class);
-	 
-	/*ANTES DE CORRER ESTA CLASE SE DEBERÁ EJECUTAR UN SCRIPT QUE SE ENCUENTRA EN LA CARPETA
-	 * CONFIG Y SE LLAMA valoresTabla.sql YA QUE AHORA PROBAMOS BÚSQUEDA SOLAMENTE, EL SCRIPT CARGARÁ 
-	 * ALGUNOS VALORES EN LA BASE DE DATOS*/
-
-    public static void main( String[] args ){
-        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/inmoapp_development", "root", "root");
-        ABMBuilding abmBuilding = new ABMBuilding();//Creo una instancia de ABM para inmuebles
-        SearchEngine searchEngine= new SearchEngine();
-              
-        //creo un inmueble con dueño ezequiel e inmobiliaria "La casa"
-        ObjectBuilding prueba2 = new ObjectBuilding("Monoambiente", "1000", "sale", "house", "Rivadavia", "125", "Centro", "Rio Cuarto", 5800,"37.134.136","Terra Nova");
-        abmBuilding.createBuilding(prueba2);
-        System.out.println("Busco inmuebles del dueño de dni 37.134.136");
-        System.out.println(searchEngine.search("37.134.136",null,null,false,false,-1,null,null));
-        System.out.println("Busco inmuebles de la inmobiliaria Capello S.A");
-        System.out.println(searchEngine.search(null,"Capello S.A",null,false,false,-1,null,null));
-        System.out.println("Busco inmuebles de tipo DEPARTAMENTO");
-        System.out.println(searchEngine.search(null,null,"departament",false,false,-1,null,null));
-        System.out.println("Busco inmuebles de tipo CASA");
-        System.out.println(searchEngine.search(null,null,"house",false,false,-1,null,null));
-        System.out.println("Busco inmuebles de dueño 37.134.136 e inmo Terra Nova");
-        System.out.println(searchEngine.search("37.134.136","Terra Nova",null,false,false,-1,null,null));
-        System.out.println("Busco inmuebles de  precio entre 15 y 100");
-        System.out.println(searchEngine.search(null,null,null,false,true,-1,"15","100"));
-        System.out.println("Busco inmuebles de precio entre 1000 y 1000");
-        System.out.println(searchEngine.search(null,null,null,false,true,-1,"1000","1000"));
-    }
+public static void main(String[] args) {
+    
+    //Lista todo los owners
+    get(new Route("/owner") {
+        @Override
+        public Object handle(Request request, Response response) {
+            Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/inmoapp_development", "root", "root");
+            Base.openTransaction();
+            String ret = "";
+            LazyList<Owner> ol = Owner.findAll();
+            Iterator<Owner> itro = ol.iterator();
+            ABMOwner ambo = new ABMOwner();
+            while (itro.hasNext()){
+                Owner ow = itro.next();
+                ObjectOwner o = ambo.consultOwner(ow.getDni());
+                ret += o.toString()+", ";
+            }
+            Base.rollbackTransaction();
+            Base.close();
+            return ret;
+        }
+    });
+    
+    //Muestra el owner del dni pasado como parametro
+    get(new Route("/owner/:dni") {
+        @Override
+        public Object handle(Request request, Response response) {
+            Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/inmoapp_development", "root", "root");
+            Base.openTransaction();
+            ABMOwner ambo = new ABMOwner();
+            ObjectOwner ow = ambo.consultOwner(request.params(":dni"));
+            String ret = ow.toString();
+            System.out.println(ret);
+            Base.rollbackTransaction();
+            Base.close();
+            return ret;
+        }
+    });
+    
+    
+    put(new Route ("/realestate/:name:/:phone_number/:web_site/:email/:city/:code/:street/:num/:neighborhood/:owners") {        
+        public Object handle(Request request, Response response) {
+            Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/inmoapp_development", "root", "root");
+            Base.openTransaction();
+            ObjectOwner ow = new ObjectOwner(request.params(":name"),request.params(":phone_number"),
+                    request.params(":web_site"),request.params(":email"),request.params(":city"),
+                    Integer.parseInt(request.params(":code")),request.params(":street"),request.params(":num"),
+                    request.params(":neighborhood"),request.params(":owners"));
+            ABMOwner ambo = new ABMOwner();
+            ambo.createOwner(ow);
+            ow = ambo.consultOwner(request.params(":dni"));
+            String ret = ow.toString();
+            Base.rollbackTransaction();
+            Base.close();
+            return ret;
+        }  
+    });
+    
+}   
 }
+
+
+
+
